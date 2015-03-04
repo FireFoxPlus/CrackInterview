5850e973bbaf4ad942b2b1e1cace8c6e7595c142
FireFoxPlus <zhangzheming1990@hotmail.com>
2015-03-04 12:23:48 +0800

    3/4

--- src/edu/crackInterview/chap8.java
+++ src/edu/crackInterview/chap8.java
@@ -12,13 +12,13 @@ public class chap8 {
     		chars[i] = 0;
     	for(int i = 0; i < val.length(); i++)
     		chars[val.charAt(i)]++;
-    		
+
     	for(int i = 0; i < 256; i++)
     		if(chars[i] > 1)
     			return false;
     	return true;
-    } 
-    
+    }
+
     //1.3
     public boolean isSame(String val1 , String val2){
     	if(val1.length() != val2.length())
@@ -32,7 +32,7 @@ public class chap8 {
     			return false;
     	return true;
     }
-    
+
     //1.4
     public String reBlank(String val){
     	int blanks = 0;
@@ -57,7 +57,7 @@ public class chap8 {
     	rs = new String(rsChar);
     	return rs;
     }
-    
+
   //1.5
     public String compressStr(String val){
     	StringBuffer buf = new StringBuffer();
@@ -67,7 +67,7 @@ public class chap8 {
     		if(i == 0){
     			buf.append(val.charAt(i));
     			continue;
-    		}   		
+    		}
     		if(val.charAt(i) != val.charAt(i - 1)){
     			buf.append(counts + 1);
     			buf.append(val.charAt(i));
@@ -85,17 +85,16 @@ public class chap8 {
     	return buf.toString();
     }
   //1.6给定N*N的矩阵表示图像，其中每个像素4字节，将图像旋转90度，不占用额外存储空间可否做到？
-    
+
   //1.7若M*N矩阵中某个元素为0，则将其所在行与列清零
   //先遍历找0，记录行列，再以此作为判别标准
-   
-    
+
+
   //1.8假定有方法isSubstring，可检查一个单词是否为其他字符串的子串，给定两个字符串s1和s2，
-  //验证s2是否由s1旋转而成，要求只能调用一次isSubstring  
+  //验证s2是否由s1旋转而成，要求只能调用一次isSubstring
     public boolean isSwap(String str1 , String str2)
     {
-    	str1 = str1 + str1 + str1;
-    	str2 = str2 + str2;
+    	str1 = str1 + str1;
     	if(str1.indexOf(str2) == -1)
     		return false;
     	else
