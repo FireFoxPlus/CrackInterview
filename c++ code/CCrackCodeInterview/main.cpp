6127fdcab8b9da0dd721f6d69190ffba7bcbb5a9
FireFoxPlus <zhangzheming1990@hotmail.com>
2015-03-05 21:35:19 +0800

    3/5 21:30

--- c++ code/CCrackCodeInterview/main.cpp
+++ c++ code/CCrackCodeInterview/main.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
-#include "cycleText.h"
+#include "isBalance.h"
 
 using namespace std;
 
 int main()
 {
-    slist<int> num1;
-    slist<int> num2;
-    slist<int> *rs;
-    cycleText<int> ob;
-    num1.tailInsert(4);
-    num1.tailInsert(0);
-    num1.tailInsert(0);
-    num1.tailInsert(4);
-    if(ob.isCycle(num1))
-        cout<<"true"<<endl;
-    else
-        cout<<"false"<<endl;
+
     return 0;
 }
