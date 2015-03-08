e36b6c68eb02192c3518d4ae71971fa1d838d1ff
FireFoxPlus <zhangzheming1990@hotmail.com>
2015-03-08 17:06:04 +0800

    3/8 17:05

--- src/edu/crackInterview/main.java
+++ src/edu/crackInterview/main.java
@@ -5,17 +5,7 @@ import java.util.Stack;
 public class main {
 
 	public static void main(String[] args) {
-		boolean[][] flag = new boolean[4][4];
-		for(int line = 0; line < 4; line++)
-			for(int col = 0; col < 4; col++)
-				flag[line][col] = false;
-		
-		int[][] graph = {{0 , 1 , 0 , 0} , {0 , 0 , 1 , 1} , {0 , 0 , 0 , 0} , {0 , 0 , 0 , 0}};
-		
-		chap8_4 ob = new chap8_4();
-		if(ob.isPath(graph, 4, 3, 4, flag))
-			System.out.println("true");
-		else
-			System.out.println("false");
+		chap8_5 ob = new chap8_5();
+		System.out.println(ob.changeLoca(31, 14));
 	}
 }
