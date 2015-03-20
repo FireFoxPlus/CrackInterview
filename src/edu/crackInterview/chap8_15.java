5e7f6a9529454cb0fd6297947af962c9b28ae66f
FireFoxPlus <zhangzheming1990@hotmail.com>
2015-03-20 08:55:54 +0800

    3/20 8:55

---
+++ src/edu/crackInterview/chap8_15.java
@@ -0,0 +1,8 @@
+package edu.crackInterview;
+
+public class chap8_15 {
+	/*
+	 *Êý¾Ý¿â 
+	 * 
+	 */
+}
---
+++ src/edu/crackInterview/chap8_16.java
@@ -0,0 +1,24 @@
+package edu.crackInterview;
+
+public class chap8_16 {
+	/*
+	 * 16.1进程和线程的区别？
+	 * 16.2如何测量上下文切换的时间
+	 * 16.3使用线程和锁，避免哲学家就餐问题中出现的死锁
+	 * 16.4设计一个类，只有在不发生死锁的情况下，才提供锁
+	 * 16.5以下代码
+	 * public class Foo
+	 * {
+	 * public Foo(){}
+	 * public void first(){}
+	 * public void second(){}
+	 * public void third(){}
+	 * }
+	 * 同一个Foo实例会被传入三个不同的线程。threadA会调用first，
+	 * thredB会调用second，threadC会调用third。设计一种机制，
+	 * 确保first在second之前调用，second在third之前调用
+	 * 16.6给定一个类，内含同步方法A和普通方法B。在同一个程序实例中，有两个线程，
+	 * 能够同时执行A？两者能否同时执行A和B?
+	 */
+	
+}
