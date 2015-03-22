978d21f33f3c5bec4d461151d55f4165f8327d34
FireFoxPlus <zhangzheming1990@hotmail.com>
2015-03-22 16:24:35 +0800

    3/22 16:24

--- c++ code/CCrackCodeInterview/main.cpp
+++ c++ code/CCrackCodeInterview/main.cpp
@@ -3,16 +3,38 @@
 #include <cstring>
 #include <time.h>
 #include <stdio.h>
+#include <algorithm>
 
+using namespace std;
 #include "swapWithoutTmp.h"
+
+
+bool compare(string val1 , string val2)
+{
+    int tmp1 = 0 , tmp2 = 0;
+    for(int i = 0; i < val1.size(); i++)
+    {
+        if(val1[i] >= '0' && val1[i] <= '9')
+            tmp1 = tmp1 * 10 + val1[i];
+        else break;
+    }
+    for(int i = 0; i < val2.size(); i++)
+    {
+        if(val2[i] >= '0' && val2[i] <= '9')
+            tmp2 = tmp2 * 10 + val2[i];
+        else break;
+    }
+    return tmp1 < tmp2;
+}
+
 
 using namespace std;
 
 int main()
 {
-    int a = 1 , b = 2;
-    swapWithOutTmp ob;
-    ob.swapNoTmp(&a , &b);
-    cout<<a<<endl<<"b"<<endl;
+    string ab[] = {"23124df" , "23vf" , "789df" , "797df" , "898sdf" , "234df"};
+    sort(ab , ab + 6 , compare);
+    for(int i = 0; i < 6; i++)
+        cout<<ab[i]<<endl;
     return 0;
 }
--- src/edu/crackInterview/main.java
+++ src/edu/crackInterview/main.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 public class main {
 
 	public static void main(String[] args) {
-		//String[] vals = {"abc" , "bca" , "ccc" , "bbc" , "bbb" , "cab"};
+		String[] vals = {"abc" , "bca" , "ccc" , "bbc" , "bbb" , "cab"};
 		int[] vals = {1 , 3 , 4 , 5 , 6 , 1};
 		chap8_11 ob = new chap8_11();
 		System.out.println(ob.found(vals , 0 , 5 , 6));
