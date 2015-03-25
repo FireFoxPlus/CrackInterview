d5981734432e350005c06504c095dc6f40a54770
FireFoxPlus <zhangzheming1990@hotmail.com>
2015-03-26 00:21:25 +0800

    3/26 0:21

--- c++ code/CCrackCodeInterview/getShortSeq.cpp
+++ c++ code/CCrackCodeInterview/getShortSeq.cpp
@@ -8,6 +8,54 @@ void getShortSeq::getSeq(int vals[] , int len)
         cout<<"left :" <<0<<"right :"<<len - 1;
         return;
     }
+    int leftIndex  = 0 , rightIndex = len - 1;
+    for(int i = 1; i < len; i++)
+    {
+        if(vals[i] < vals[i - 1])
+        {
+            leftIndex = i - 1;
+            break;
+        }
+        leftIndex++;
+    }
+    for(int i = len - 2; i >= 0; i--)
+    {
+        if(vals[i] > vals[i + 1])
+        {
+            rightIndex = i + 1;
+            break;
+        }
+        rightIndex--;
+    }
+    if(leftIndex == len - 1 && rightIndex == 0)
+    {
+        cout<<"already Sorted!"<<endl;
+        return;
+    }
+    int minMid = vals[leftIndex + 1], maxMid = vals[rightIndex - 1];
+    for(int i = leftIndex; i <= rightIndex; i++)
+    {
+        if(vals[i] < minMid)
+            minMid = vals[i];
+        if(vals[i] > maxMid)
+            maxMid = vals[i];
+    }
+    for(int i = leftIndex; i >=0; i--)
+    {
+        if(vals[i] < minMid)
+        {
+            leftIndex = i + 1;
+            break;
+        }
+    }
+    for(int i = rightIndex; i < len; i++)
+    {
+        if(vals[i] > maxMid)
+        {
+            rightIndex = i - 1;
+            break;
+        }
 
-
+    }
+    cout<<"left:"<<leftIndex<<endl<<"right:"<<rightIndex<<endl;
 }
--- c++ code/CCrackCodeInterview/main.cpp
+++ c++ code/CCrackCodeInterview/main.cpp
@@ -3,61 +3,13 @@
 #include <string>
 #include <stdio.h>
 #include <algorithm>
-#include "guessColor.h"
+#include "getShortSeq.h"
 using namespace std;
-int FindSubString( char* pch )
-{
-    int   count  = 0;
-    char  * p1   = pch;
-    while ( *p1 != '\0' )
-    {
-        if ( *p1 == p1[1] - 1 )
-        {
-            p1++;
-            count++;
-        }else  {
-            break;
-        }
-    }
-    int count2 = count;
-    while ( *p1 != '\0' )
-    {
-        if ( *p1 == p1[1] + 1 )
-        {
-            p1++;
-            count2--;
-        }else  {
-            break;
-        }
-    }
-    if ( count2 == 0 )
-        return(count);
-    return(0);
-}
-void ModifyString( char* pText )
-{
-    char  * p1   = pText;
-    char  * p2   = p1;
-    while ( *p1 != '\0' )
-    {
-        int count = FindSubString( p1 );
-        if ( count > 0 )
-        {
-            *p2++ = *p1;
-            sprintf( p2, "%i", count );
-            while ( *p2 != '\0' )
-            {
-                p2++;
-            }
-            p1 += count + count + 1;
-        }else  {
-            *p2++ = *p1++;
-        }
-    }
-}
+
 int main( void )
 {
-    guessColor ob;
-    ob.guessResult("YYYB" , "BYYY");
+    int vals[] = {1,2 ,4 , 5 ,3};
+    getShortSeq ob;
+    ob.getSeq(vals , 5);
     return 0;
 }
