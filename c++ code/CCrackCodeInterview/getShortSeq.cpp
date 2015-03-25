c294140fd3b8a5632440c8986f826eff5620ea3f
FireFoxPlus <zhangzheming1990@hotmail.com>
2015-03-25 21:40:56 +0800

    3/25 21:40

--- c++ code/CCrackCodeInterview/getShortSeq.cpp
+++ c++ code/CCrackCodeInterview/getShortSeq.cpp
@@ -3,5 +3,11 @@
 using namespace std;
 void getShortSeq::getSeq(int vals[] , int len)
 {
+    if(vals[0] > vals[len - 1])
+    {
+        cout<<"left :" <<0<<"right :"<<len - 1;
+        return;
+    }
+
 
 }
